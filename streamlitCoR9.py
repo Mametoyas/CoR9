b6d9071c2492892afe0ad42416174f7240f9dea0
Mametoyas <163328433+Mametoyas@users.noreply.github.com>
2025-08-16 04:43:57 +0700

    Update streamlitCoR9.py

--- streamlitCoR9.py
+++ streamlitCoR9.py
@@ -34,6 +34,7 @@ css_content = """
     /* Main body and app container */
     body {
         background: url("https://images.pexels.com/photos/716656/pexels-photo-716656.jpeg");
+        filter: blur(5px);
         background-size: cover;
     }
     .stApp {
