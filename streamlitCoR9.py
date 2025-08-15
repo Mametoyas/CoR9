7bd68de13fb2f6b7f8350eafb7b86945639e60be
Mametoyas <163328433+Mametoyas@users.noreply.github.com>
2025-08-16 05:03:22 +0700

    Update streamlitCoR9.py

--- streamlitCoR9.py
+++ streamlitCoR9.py
@@ -101,6 +101,9 @@ css_content = """
         padding-top: 2rem;
         padding-bottom: 2rem;
     }
+    .space {
+    margin-top: 30px;  /* adjust as needed */
+}
 """
 with open("style.css", "w") as f:
     f.write(css_content)
@@ -194,7 +197,7 @@ def plot_yolo_result_image_and_analyze(image_data, results):
         )
     
     # Use st.pyplot() to display the Matplotlib figure
-    st.markdown("<br><br>")
+    st.markdown('<div class="space"></div>', unsafe_allow_html=True)
     st.pyplot(fig)
     st.markdown("<div class='analysis-container'>", unsafe_allow_html=True)
     st.subheader("📊 Analysis Results")
